e0ee992323b62572cbdbf6a59991f2ff5c95839c
jswalker808 <seiyawalk@gmail.com>
2018-04-18 08:43:31 -0600

    Added add movie functionality

---
+++ src/App.vue
@@ -0,0 +1,82 @@
+<template>
+  <div id="app">
+    <app-header/>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+ import AppHeader from './components/AppHeader';
+ export default {
+   name: 'App',
+   components: { AppHeader }
+ }
+</script>
+
+<style>
+ /* https://color.adobe.com/CS04-color-theme-1994456/?showPublished=true */
+ body {
+     font-size: 18px;
+     padding: 20px 100px 0px 100px;
+     font-family: 'Avenir', Helvetica, Arial, sans-serif;
+     -webkit-font-smoothing: antialiased;
+     -moz-osx-font-smoothing: grayscale;
+ }
+ button {
+     color: #fff;
+     min-width: 100px;
+     line-height: 1em;
+     border: none;
+     border-radius: 5px;
+     color: white;
+     padding: 5px;
+     text-align: center;
+     text-decoration: none;
+     display: inline-block;
+     font-size: 1rem;
+     margin: 4px 2px;
+     height: 15%;
+ }
+ button.primary {
+     background-color: #4ABDAC;
+ }
+ button.alternate {
+     background-color: #4ABDAC;
+     margin-top: 20px;
+ }
+ button:focus {
+     outline: none;
+ }
+ button:hover {
+
+ }
+ button:active {
+     transform: translateY(4px);
+ }
+ input {
+     height: 20px;
+     padding: 10px;
+     border-radius: 5px;
+ }
+ a {
+     color: #77C4D3;
+ }
+ .flexWrapper {
+     display:flex;
+ }
+ .flexRight {
+     margin-left: auto;
+ }
+ .error {
+     color: #F35537;
+ }
+ .column {
+     width: 100%;
+ }
+ .feed {
+     width: 100px;
+ }
+ .feed img {
+     width: 100%;
+ }
+</style>
---
+++ src/components/UserFeed.vue
@@ -0,0 +1,136 @@
+<template>
+  <div class="feed">
+    <div>
+      <h3>Enter a movie into your watch list!</h3>
+      <form v-on:submit.prevent="tweet" class="tweetForm">
+	<input v-model="text" placeholder="Movie Name"/>
+  <button class="primary" type="submit">Submit</button>
+	<div class="buttonWrap">
+	</div>
+      </form>
+    </div>
+    <div class="itemSection">
+    <div v-for="item in feed" class="item">
+      <p class="tweet">{{item.tweet}}</p>
+    </div>
+    </div>
+  </div>
+</template>
+
+<script>
+ import moment from 'moment';
+ export default {
+   name: 'UserFeed',
+   data () {
+     return {
+       text: '',
+     }
+   },
+   created: function() {
+     this.$store.dispatch('getFeed');
+   },
+   filters: {
+     since: function(datetime) {
+       moment.locale('en', {
+	 relativeTime: {
+	   future: 'in %s',
+	   past: '%s',
+	   s:  'seconds',
+	   ss: '%ss',
+	   m:  '1m',
+	   mm: '%dm',
+	   h:  'h',
+	   hh: '%dh',
+	   d:  'd',
+	   dd: '%dd',
+	   M:  ' month',
+	   MM: '%dM',
+	   y:  'a year',
+	   yy: '%dY'
+	 }
+       });
+       return moment(datetime).fromNow();
+     },
+   },
+   computed: {
+     feed: function() {
+       return this.$store.getters.feed;
+     },
+   },
+   methods: {
+     tweet: function() {
+       this.$store.dispatch('addTweet',{
+         tweet: this.text,
+       }).then(tweet => {
+	       this.text = "";
+       });
+     },
+   }
+ }
+</script>
+
+<style scoped>
+ .feed {
+     width: 600px;
+ }
+ .tweetForm {
+     padding: 10px;
+     margin-bottom: 10px;
+     border-radius: 5px;
+ }
+ .buttonWrap {
+     width: 100%;
+     display: flex;
+ }
+ button {
+     margin-top: 1%;
+     margin-left: 3%;
+     height: 35%;
+     font-size: 0.9em;
+     padding: 10px;
+ }
+ textarea {
+     width: 100%;
+     height: 5em;
+     padding: 2px;
+     margin-bottom: 5px;
+     resize: none;
+     box-sizing: border-box;
+ }
+ span button {
+  min-width: 20px;
+  background-color: #ff4949;
+  float: right;
+ }
+ input {
+    border-radius: 5px;
+ }
+ .item {
+     border-bottom: 1px solid #ddd;
+     padding: 10px;
+ }
+ .itemSection {
+     border: solid 2px grey;
+     border-radius: 5px;
+     width: 75%;
+ }
+ .tweet {
+     margin-top: 0px;
+     font-weight: 800;
+ }
+ .idline {
+     margin-bottom: 0px;
+ }
+ .user {
+     font-weight: bold;
+     margin-right: 10px;
+ }
+ .handle {
+     margin-right: 10px;
+     color: #666;
+ }
+ .time {
+     float: right;
+     color: #666;
+ }
+</style>
